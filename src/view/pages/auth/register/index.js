136c6db9d22440588cee00a840d4287636f4ced3
Mariam0422 <mariam79.yan@gmail.com>
2024-08-05 20:10:25 +0400

    change class component

--- src/view/pages/auth/register/index.js
+++ src/view/pages/auth/register/index.js
@@ -1,78 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Input, Button, Divider, Form, notification, Flex } from "antd";
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth, setDoc, doc, db } from "../../../../services/firebase/firebase";
 import AuthWrapper from '../../../components/shared/AuthWrapper';
 import RegisterCoverImg from '../../../../core/images/registerCover.png';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const { Title,Text } = Typography;
-export default class Register extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      loading: false,
-      firstName: "",
-      lastName: "",
-      headLine: "",
-      email: "",
-      password: ""
-    };
 
-    this.handleRegister = this.handleRegister.bind(this);
-  }
-  handleChangeInput = (e) => {
-    this.setState(e)
-  };
- async handleRegister() {
-    const { email, password, firstName, lastName, headLine } = this.state;
-    this.setState({
-        loading: true
-    });
-      try{
-     const response = await createUserWithEmailAndPassword(auth, email, password); 
+const Register = ()  => {
+  const [ form ] = Form.useForm();
+  const navigate = useNavigate();
+  const [ loading, setLoading ] = useState(false);
+
+  const handleRegister = async (values) => {
+    setLoading(true);
+    try{
+     const { email, password, ...restData } = values;
+     const response = await createUserWithEmailAndPassword(auth, email, password);
      const uid = response.user.uid;
-     const createDoc = doc(db, "registerUsers", uid);
-     setDoc(createDoc, {
-      firstName, lastName, headLine, email
-     })     
-        notification.success({
-            message: "Success Registration",
-            description: `Welcome ${firstName} ${lastName}`
-        })        
-      }catch(error){
-   notification.error({
-    message: "Wrong Registration",
-    description: "Ooooooooops"
-   })
-      }finally{
-         this.setState({
-            loading: false
-         })
-      }
-   
+     const createDoc = doc(db, 'registerUsers', uid);
+     await setDoc(createDoc, {
+      email, ...restData
+     });
+     navigate("/login");
+    }catch{
+      notification.error({
+        message: "Wrong Registration",
+        description: "Ooooooooops"
+       })
+    }finally{
+    setLoading(false);
+    }
   }
-  render() {
-        return (
+  return(
       <AuthWrapper coverImg={RegisterCoverImg}>
         <Title level={2}>
           Register
           </Title>
-        <Form layout="vertical" onValuesChange={this.handleChangeInput}>
-          <Form.Item label="First Name" name="firstName">
+        <Form form={form} layout="vertical" onFinish={handleRegister}>
+          <Form.Item 
+          name="firstName"
+          label="First Name" 
+          rules={[
+            {
+              required: true,
+              message: 'First Name is required!'
+            }
+          ]}
+          >
             <Input type="text" placeholder="Firts Name" />
           </Form.Item>
-          <Form.Item label="Last Name" name="lastName">
+          <Form.Item           
+          name="lastName"
+          label="Last Name" 
+          rules={[
+            {
+              required: true,
+              message: 'Last Name is required!'
+            }
+          ]}>
             <Input type="text" placeholder="Last Name" />
           </Form.Item>
-          <Form.Item label="headLine" name="headLine">
+          <Form.Item        
+          name="headLine"
+          label="HeadLine" 
+          rules={[
+            {
+              required: true,
+              message: 'HeadLine is required!'
+            }
+          ]}
+          >
             <Input type="text" placeholder="HeadLine" />
           </Form.Item>
-          <Form.Item label="email" name="email">
+          <Form.Item          
+            name="email"
+            label="Email" 
+            rules={[
+            {
+              required: true,
+              message: 'Email is required!'
+            }
+          ]}>
             <Input type="email" placeholder="Email" />
           </Form.Item>
-          <Form.Item label="password" name="password">
-            <Input.Password  placeholder="Password" />
+          <Form.Item              
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: 'Password is required!'
+              }
+            ]}>
+          <Input.Password  placeholder="Password" />
           </Form.Item>
           <Divider />
          <Flex justify="space-between" align="flex-end">
@@ -83,14 +105,14 @@ export default class Register extends React.Component {
           </Text>
           
          <Button type="primary"
-          onClick={this.handleRegister}
-          loading={this.state.loading}>
+         htmlType="submit"       
+         loading={loading}>
           Register
           </Button>
          </Flex>
        
         </Form>
         </AuthWrapper>
-    );
-  }
+  )
 }
+export default Register; 
