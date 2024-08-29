cd2c809afe5c5fe87d22855120b9ae1ace00573d
Mariam0422 <mariam79.yan@gmail.com>
2024-08-29 21:47:28 +0400

    implement all drag functionality

--- src/App.js
+++ src/App.js
@@ -1,69 +1,123 @@
-import {useState, useEffect} from 'react';
-import { CabinetLayout, MainLayout } from './view/layouts';
-import {Login, Register} from './view/pages/auth';
-import { db, auth, getDoc, onAuthStateChanged, doc} from './services/firebase/firebase';
-import { AuthContextProvider } from './context/AuthContext';
-import LoadingWrapper from './view/components/shared/LoadingWrapper';
-import { ROUTES_CONSTANTS } from './routes';
-import CabinetBoard from './view/pages/cabinetBoard';
+import { useState, useEffect } from "react";
+import { CabinetLayout, MainLayout } from "./view/layouts";
+import { Login, Register } from "./view/pages/auth";
+import {
+  db,
+  auth,
+  getDoc,
+  onAuthStateChanged,
+  doc,
+  getDocs,
+  collection
+} from "./services/firebase/firebase";
+import { AuthContextProvider } from "./context/AuthContext";
+import LoadingWrapper from "./view/components/shared/LoadingWrapper";
+import { ROUTES_CONSTANTS } from "./routes";
+import CabinetBoard from "./view/pages/cabinetBoard";
+import { taskStatusModel } from "./view/pages/cabinetBoard/constants"; //todo
 import {
   Route,
-  createBrowserRouter, 
-  createRoutesFromElements, 
+  createBrowserRouter,
+  createRoutesFromElements,
   RouterProvider,
-  Navigate 
-} from 'react-router-dom'; 
-import './App.css';
-
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
 
 const App = () => {
-const [isAuth, setIsAuth] = useState(false);
-const [loading, setLoading] = useState(false);
-const [userProfileInfo, setUserProfileInfo] =  useState({
-  firstName: '',
-  lastName: '',
-  headLine: '',
-  email: '',
-});
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [columns, setColumns] = useState(taskStatusModel); //todo
+  const [issueLoading, setissueLoading] = useState(false)//todo
+  const [userProfileInfo, setUserProfileInfo] = useState({
+    firstName: "",
+    lastName: "",
+    headLine: "",
+    email: "",
+  });
+  const hadleGetIssues = async () => { //todo
+    setissueLoading(true)
+    const updatedTaskStatusModel = taskStatusModel();
+    const queryData = await getDocs(collection(db, "issue"));
+    queryData.docs.map((doc) => {
+      const data = doc.data();
+      const { status } = data;
+      if (updatedTaskStatusModel[status]) {
+        updatedTaskStatusModel[status].items.push(data);
+      }
+    });
 
+    setColumns({ ...updatedTaskStatusModel });
+    setissueLoading(false)
+  };
+  useEffect(() => {
+    setLoading(true);//todo
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
 
-useEffect(() => {
-  setLoading(true);
-  onAuthStateChanged(auth, (user) =>{ 
-  setLoading(false)  
+      if (user) {
+        setIsAuth(true);
+        const { uid } = user;
+        const ref = doc(db, "registerUsers", uid);
+        getDoc(ref).then((userData) => {
+          if (userData.exists()) {
+            setUserProfileInfo(userData.data());
+          }
+        });
+      }
+    });
+  }, []);
 
-   if(user){     
-    setIsAuth(true)
-    const {uid} = user;
-    const ref = doc(db, 'registerUsers', uid);
-    getDoc(ref).then((userData) => {
-      if(userData.exists()){
-        setUserProfileInfo(userData.data());    
-      }  
-    })
-   } 
-})
-}, [])
-
-
-return (
-  <LoadingWrapper loading={loading} fullScreen>
-    <AuthContextProvider value={{isAuth, userProfileInfo, setIsAuth}}>
-    <RouterProvider router={ createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout/>}>
-      <Route path={ROUTES_CONSTANTS.LOGIN} element={!isAuth ? <Login/> : <Navigate to={ROUTES_CONSTANTS.CABINET}/>}/>
-      <Route path={ROUTES_CONSTANTS.REGISTER} element={!isAuth ? <Register/> : <Navigate to={ROUTES_CONSTANTS.CABINET}/>}/>
-      {/* *------Cabinet Layout Route -------* */}
-      <Route path={ROUTES_CONSTANTS.CABINET} element={isAuth ? <CabinetLayout/> : <Navigate to={ROUTES_CONSTANTS.LOGIN}/>} >
-      <Route path={ROUTES_CONSTANTS.CABINET} element={<CabinetBoard/>}/>
-      </Route>
-    </Route>
-  )
-)}/>
-    </AuthContextProvider>
-  </LoadingWrapper>
-)
-}
+  return (
+    <LoadingWrapper loading={loading} fullScreen>
+      <AuthContextProvider value={{ isAuth, userProfileInfo, setIsAuth, issueLoading, columns,setColumns, hadleGetIssues }}>
+        <RouterProvider
+          router={createBrowserRouter(
+            createRoutesFromElements(
+              <Route path="/" element={<MainLayout />}>
+                <Route
+                  path={ROUTES_CONSTANTS.LOGIN}
+                  element={
+                    !isAuth ? (
+                      <Login />
+                    ) : (
+                      <Navigate to={ROUTES_CONSTANTS.CABINET} />
+                    )
+                  }
+                />
+                <Route
+                  path={ROUTES_CONSTANTS.REGISTER}
+                  element={
+                    !isAuth ? (
+                      <Register />
+                    ) : (
+                      <Navigate to={ROUTES_CONSTANTS.CABINET} />
+                    )
+                  }
+                />
+                {/* *------Cabinet Layout Route -------* */}
+                <Route
+                  path={ROUTES_CONSTANTS.CABINET}
+                  element={
+                    isAuth ? (
+                      <CabinetLayout />
+                    ) : (
+                      <Navigate to={ROUTES_CONSTANTS.LOGIN} />
+                    )
+                  }
+                >
+                  <Route
+                    path={ROUTES_CONSTANTS.CABINET}
+                    element={<CabinetBoard />}
+                  />
+                </Route>
+              </Route>
+            )
+          )}
+        />
+      </AuthContextProvider>
+    </LoadingWrapper>
+  );
+};
 
 export default App;
--- src/view/components/shared/CreateIssueModal/index.js
+++ src/view/components/shared/CreateIssueModal/index.js
@@ -1,132 +1,149 @@
-import { useState } from 'react';
-import { Modal, Form, Input, Select, notification } from 'antd';
-import { issueTypes, priority, taskStatus } from '../../../../core/constant/issue';
-import Editor from '../Editor';
-import { doc, setDoc, db, updateDoc, arrayUnion } from '../../../../services/firebase/firebase';
+import { useState, useContext } from "react";
+import { Modal, Form, Input, Select, notification } from "antd";
+import {
+  issueTypes,
+  priority,
+  taskStatus,
+} from "../../../../core/constant/issue";
+import Editor from "../Editor";
+import {
+  doc,
+  setDoc,
+  db,
+  updateDoc,
+  arrayUnion,
+} from "../../../../services/firebase/firebase";
+import { AuthContext } from "../../../../context/AuthContext";
 
-const CreateIssueModal = ({visible, setVisible, users}) => {
+const CreateIssueModal = ({ visible, setVisible, users }) => {
   const [form] = Form.useForm();
   const [confirmLoading, setconfirmLoading] = useState(false);
- 
-    const handleUpdateAssigneesTask = async(taskId,  assignerId) => {
-     const docRef = doc(db, 'registerUsers', assignerId);
-     await updateDoc(docRef, {
-      task: arrayUnion(taskId)
-     })
-    }
+  const { hadleGetIssues } = useContext(AuthContext);
 
-    const handleCloseModal = () => {      
-        setVisible(false);
-        form.resetFields();
-    }
-    const handleCreateIssue = async(values) => {
-      const taskId = `${Date.now()}`;
-      setconfirmLoading(true);
-      const taskDataModel = {
-        key: taskId,
-        status: taskStatus.TODO.key,
-        ...values
-      }
-    
-      try{       
-        const createDoc =  doc(db, "issue", taskId);
-        await setDoc(createDoc, taskDataModel);
-        await handleUpdateAssigneesTask(taskId, values.assignees);
-        setVisible(false);
-        form.resetFields();
-        notification.success({
-          message: "Your task has been created",          
-        })
-      }
-      catch(error){
-         notification.error({
-          message: "Error ooops :("
-         });
-      }finally{
+  const handleUpdateAssigneesTask = async (taskId, assignerId) => {
+    const docRef = doc(db, "registerUsers", assignerId);
+    await updateDoc(docRef, {
+      task: arrayUnion(taskId),
+    });
+  };
+
+  const handleCloseModal = () => {
+    setVisible(false);
+    form.resetFields();
+  };
+  const handleCreateIssue = async (values) => {
+    const taskId = `${Date.now()}`;
+    setconfirmLoading(true);
+    const taskDataModel = {
+      key: taskId,
+      status: taskStatus.TODO.key,
+      ...values,
+    };
+
+    try {
+      const createDoc = doc(db, "issue", taskId);
+      await setDoc(createDoc, taskDataModel);
+      await handleUpdateAssigneesTask(taskId, values.assignees);
+      setVisible(false);
+      form.resetFields();
+      hadleGetIssues();
+      notification.success({
+        message: "Your task has been created",
+      });
+    } catch (error) {
+      notification.error({
+        message: "Error ooops :(",
+      });
+    } finally {
       setconfirmLoading(false);
-      }
-      
     }
- return(
-   <Modal
-   title="Create Issue"
-   okText="Create Issue"
-   centered
-   open={visible}
-   width={800}
-   onCancel={handleCloseModal}
-   onOk={form.submit}
-   confirmLoading={confirmLoading}
-   styles={{
-    body:{
-      maxHeight: '600px',
-      overflowY: 'auto',
-      overflowX: 'hidden'
-    }
-   }}
-   >
-    <Form layout="vertical" form={form} onFinish={handleCreateIssue}>
-        <Form.Item 
+  };
+  return (
+    <Modal
+      title="Create Issue"
+      okText="Create Issue"
+      centered
+      open={visible}
+      width={800}
+      onCancel={handleCloseModal}
+      onOk={form.submit}
+      confirmLoading={confirmLoading}
+      styles={{
+        body: {
+          maxHeight: "600px",
+          overflowY: "auto",
+          overflowX: "hidden",
+        },
+      }}
+    >
+      <Form layout="vertical" form={form} onFinish={handleCreateIssue}>
+        <Form.Item
           name="issueType"
           label="Issue Type"
-          rules={[{required: true, message: "Plese select Issue Type"}]}>
-          <Select 
-          showSearch
-          placeholder="Issue Type"
-          options={issueTypes}
-          />
+          rules={[{ required: true, message: "Plese select Issue Type" }]}
+        >
+          <Select showSearch placeholder="Issue Type">
+            {issueTypes.map((item) => {
+              return (
+                <Select.Option value={item.value}>
+                  {item.icon}
+                  {" "} 
+                  {item.label}
+                </Select.Option>
+              );
+            })}
+          </Select>
         </Form.Item>
-
 
         <Form.Item
-        name="shortSummary"
-        label=" Short Summary"
-        rules={[{required: true, message: "Plese input short summary"}]}
-        >        
-        <Input placeholder="Short Summary"/>
+          name="shortSummary"
+          label=" Short Summary"
+          rules={[{ required: true, message: "Plese input short summary" }]}
+        >
+          <Input placeholder="Short Summary" />
         </Form.Item>
 
-
-        <Form.Item label="Description" name="description" rules={[{required:true, message: "Please input Description"}]}>
-         <Editor/>
+        <Form.Item
+          label="Description"
+          name="description"
+          rules={[{ required: true, message: "Please input Description" }]}
+        >
+          <Editor />
         </Form.Item>
-        
-        <Form.Item 
+
+        <Form.Item
           name="reporter"
           label="Reporter"
-          rules={[{required: true, message: "Plese select Reporter"}]}>
-          <Select 
-          showSearch
-          placeholder="Reporter"
-          options={users}
-          />
+          rules={[{ required: true, message: "Plese select Reporter" }]}
+        >
+          <Select showSearch placeholder="Reporter" options={users} />
         </Form.Item>
 
-        <Form.Item 
+        <Form.Item
           name="assignees"
           label="Assignees"
-          rules={[{required: true, message: "Plese select Assignees"}]}>
-          <Select 
-          showSearch
-          placeholder="Assignees"
-          options={users}
-          />
+          rules={[{ required: true, message: "Plese select Assignees" }]}
+        >
+          <Select showSearch placeholder="Assignees" options={users} />
         </Form.Item>
 
-        <Form.Item 
+        <Form.Item
           name="priority"
           label="Priority"
-          rules={[{required: true, message: "Plese select priority"}]}>
-          <Select 
-          showSearch
-          placeholder="Priority"
-          options={priority}
-          />
+          rules={[{ required: true, message: "Plese select priority" }]}
+        >
+          <Select showSearch placeholder="Priority">
+            {priority.map((item) => {
+              return (
+                <Select.Option value={item.value}>
+                  {item.icon} {item.label}
+                </Select.Option>
+              );
+            })}
+          </Select>
         </Form.Item>
-
-
-    </Form>
-   </Modal>
- )
-}
+      </Form>
+    </Modal>
+  );
+};
 export default CreateIssueModal;
